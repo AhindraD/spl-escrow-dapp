be40294e080b8a7a939b1de4e4437fe3d5c10e3a
Ahindra D <official.ahindra@gmail.com>
2025-03-20 23:54:44 +0530

    take

--- anchor/programs/splescrowdapp/src/instructions/take_offer.rs
+++ anchor/programs/splescrowdapp/src/instructions/take_offer.rs
@@ -1,1 +1,28 @@
+use anchor_lang::prelude::*;
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    token_interface::{Mint, TokenInterface},
+};
 
+#[derive(Accounts)]
+pub struct TakeOffer<'info> {
+    #[account(mut)]
+    pub taker: Signer<'info>,
+
+    #[account(mut)]
+    pub maker: SystemAccount<'info>,
+
+    #[account(
+        mint::token_program=token_program
+    )]
+    pub token_mint_a: InterfaceAccount<'info, Mint>,
+
+    #[account(
+        mint::token_program=token_program
+    )]
+    pub token_mint_b: InterfaceAccount<'info, Mint>,
+
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub token_program: Interface<'info, TokenInterface>,
+    pub system_program: Program<'info, System>,
+}
