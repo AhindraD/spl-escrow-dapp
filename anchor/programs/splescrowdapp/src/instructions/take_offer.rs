e0b56e2cd5bfb71213cd0d2a3fc29c8e85dddad7
Ahindra D <official.ahindra@gmail.com>
2025-03-20 23:59:46 +0530

    take -strsuct

--- anchor/programs/splescrowdapp/src/instructions/take_offer.rs
+++ anchor/programs/splescrowdapp/src/instructions/take_offer.rs
@@ -1,8 +1,10 @@
 use anchor_lang::prelude::*;
 use anchor_spl::{
     associated_token::AssociatedToken,
-    token_interface::{Mint, TokenInterface},
+    token_interface::{Mint, TokenAccount, TokenInterface},
 };
+
+use crate::OfferState;
 
 #[derive(Accounts)]
 pub struct TakeOffer<'info> {
@@ -21,6 +23,28 @@ pub struct TakeOffer<'info> {
         mint::token_program=token_program
     )]
     pub token_mint_b: InterfaceAccount<'info, Mint>,
+
+    #[account(
+        mut,
+        has_one=token_mint_a,
+        has_one=token_mint_b,
+        close=maker,
+        seeds=[
+            b"offer",
+            maker.key().as_ref(),
+            offer.seed.to_le_bytes().as_ref(),
+        ],
+        bump=offer.bump
+    )]
+    pub offer: Account<'info, OfferState>,
+
+    #[account(
+        mut,
+        associated_token::authority=offer,
+        associated_token::mint=token_mint_a,
+        associated_token::token_program=token_program,
+    )]
+    pub vault: InterfaceAccount<'info, TokenAccount>,
 
     pub associated_token_program: Program<'info, AssociatedToken>,
     pub token_program: Interface<'info, TokenInterface>,
